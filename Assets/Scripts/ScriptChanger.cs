a23c675e0e9b860430884a403c7390fd7f8d5d7e
DISA <DISA@OA-S141-19>
2026-01-20 10:47:40 -0500

    fun with arrays

--- Assets/Scripts/ScriptChanger.cs
+++ Assets/Scripts/ScriptChanger.cs
@@ -6,19 +6,23 @@ public class ScriptChanger : MonoBehaviour
 {
     public SpriteRenderer SpriteRenderer;
     public Color col;
+    public Sprite[] barrels;
+    public int randomNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //PickARandomColor();
+        PickARandonSprite();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(Keyboard.current.anyKey.wasPressedThisFrame)
-        //{
-        //    PickARandomColor();
-        //}
+        if(Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            //    PickARandomColor();
+            PickARandonSprite();
+        }
 
         //get mouse position
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
@@ -33,6 +37,11 @@ public class ScriptChanger : MonoBehaviour
             //N: set to white
             SpriteRenderer.color = Color.white;
         }
+        
+        if(Mouse.current.leftButton.wasPressedThisFrame == true)
+        {
+           
+        }
   
     
     
@@ -40,5 +49,16 @@ public class ScriptChanger : MonoBehaviour
     void PickARandomColor()
     {
         SpriteRenderer.color = Random.ColorHSV();
+    }
+
+    void PickARandonSprite()
+    {
+        //SpriteRenderer.sprite = mySprite;
+        
+        //pick a random number
+        randomNumber = Random.Range(0,barrels.Length);
+        //use that number to choose a sprite
+        //asign that sprite
+       SpriteRenderer.sprite = barrels[randomNumber];
     }
 }
