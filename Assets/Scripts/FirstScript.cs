a87450fed42d58d0f925bc914eb84ef79febc79c
Benjamin Piatkowski <bpiatkowski02@gmail.com>
2026-02-04 13:53:52 -0500

    2-1 bool script

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class FirstScript : MonoBehaviour
 {
+    public bool mouseIsOverMe = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,25 @@ public class FirstScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        float distance = Vector2.Distance(transform.position, mousePos);
+
+        if(distance < 1)
+        {
+            mouseIsOverMe = true;
+        }
+        else
+        {
+            mouseIsOverMe = false;
+        }
+
+        if (mouseIsOverMe)
+        {
+            transform.localScale = new Vector3(2f, 2f, 2f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 }
